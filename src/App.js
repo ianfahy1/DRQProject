3398d20ca5e66edcaa8081930338616f69c5b4f5
Ian Fahy <ianfahy1@gmail.com>
2024-12-11 19:56:48 +0000

    Imoorted bootstrap styling,  added routing to list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Content from './components/content';
+import List from './components/list';
+
 
 
 function App() {
@@ -8,7 +11,8 @@ function App() {
     <Router>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<Content />} />
+        <Route path="/" element={<Content/>}/>
+        <Route path="/list" element={<List/>}/>
       </Routes>
     </Router>
   );
