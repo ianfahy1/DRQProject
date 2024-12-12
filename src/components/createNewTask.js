61d265837fff87b8033f4962fe3b847f20e5fb35
Ian Fahy <ianfahy1@gmail.com>
2024-12-12 15:22:29 -0300

    Updated inputs to be spaced evenly and allow for inputs of Task details and task deadlines.

--- src/components/createNewTask.js
+++ src/components/createNewTask.js
@@ -17,8 +17,24 @@ const CreateTask = () => {
         <div>
             <h1>CREATE TASK</h1>
             <form onSubmit={submit}>
-                <label>Enter Task Name</label>
-                <input type="text" value={taskName} onChange={(x)=>{setName(x.target.value)}}/>
+                <div>
+                    <label>Enter Task Name:</label>
+                    <input type="text" placeholder="Enter Task Name" onChange={(x)=>{setName(x.target.value)}}/>
+                </div>
+
+                <div>
+                    <label>Enter Task Details:</label>
+                    <input type="text" placeholder="Enter Task Details" onChange={(x)=>{setDetails(x.target.value)}}/>
+                </div>
+                
+                <div>
+                    <label>Enter Task Deadline:</label>
+                    <input type="date" onChange={(x)=>{setDeadline(x.target.value)}}/>
+                </div>
+                
+                <div>
+                    <input type="submit" value="Set Task" />
+                </div>
             </form>
         </div>
     )
